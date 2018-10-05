46b02459dbd1be370724cf6a62021044b9d0555d
Leonardo DeLeon <lqdeleon@gmail.com>
2018-10-04 22:43:31 -0500

    initial commit

---
+++ src/com/revature/Customers.java
@@ -0,0 +1,24 @@
+package com.revature;
+
+import java.util.Hashtable;
+import java.io.Serializable;
+
+class Customers implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public Hashtable<String, String> contents = new Hashtable<>();
+
+    public Hashtable<String, String> getContents() {
+        return this.contents;
+    }
+    public Customers (String username, String password) {
+        this.contents.put(username,password);
+    }
+ 
+    public static void main (String [] args) {
+
+    }
+    
+}
+
